4b07db03fcdbe9b4c7b91777b7ad7fc1705764b4
Manuel Valls Fernández <manolo@vlrz.es>
2015-03-14 14:11:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Help
+
+## Sample usage
+
+```javascript
+var help = require('u-help');
+
+process.stdout.write(help('feed <animal> <food>','Keep your pet fed!',{
+  Animals: {
+    whiskers: 'Your beloved cat',
+    snout: 'Your loyal dog',
+    scales: 'Your awesome fish',
+    feathers: 'Your annoying bird',
+    toothy: 'Your good old rodent'
+  },
+  Foods: {
+    meat: 'Carnivores\' favourite food',
+    fish: 'That weird thing which inhabits seas',
+    bones: 'These are under your skin, you know?',
+    fishbones: 'Fish would be the perfect food without them',
+    worms: 'Tasty little insects'
+  }
+}));
+```
+
+```
+
+·----------------------·
+| feed <animal> <food> |
+·----------------------·
+
+Keep your pet fed!
+
+Animals:
+
+  whiskers   Your beloved cat
+  snout      Your loyal dog
+  scales     Your awesome fish
+  feathers   Your annoying bird
+  toothy     Your good old rodent
+
+Foods:
+
+  meat        Carnivores' favourite food
+  fish        That weird thing which inhabits seas
+  bones       These are under your skin, you know?
+  fishbones   Fish would be the perfect food without them,
+              it's a real shame they exist
+  worms       Tasty little insects
+
+```
---
+++ main.js
@@ -0,0 +1,76 @@
+
+/*
+ * help('asdasd','Feed your animal',{
+ *  Commands: {
+ *    build: 'Build andas dasdasd'
+ *  }
+ * });
+ */
+
+function spaces(n){
+  var i,txt = '';
+  
+  for(i = 0;i < n;i++) txt += ' ';
+  return txt;
+}
+
+module.exports = function(command,desc,info){
+  var txt = '',
+      i,j,k,hyphons,k1,k2,ml = 0;
+  
+  if(typeof desc != 'string' && !(desc instanceof Array)){
+    info = desc;
+    desc = undefined;
+  }
+  
+  hyphons = '·-';
+  for(i = 0;i < command.length;i++) hyphons += '-';
+  hyphons += '-·';
+  
+  txt +=  '\n' + 
+          hyphons + '\n' +
+          '| ' + command + ' |\n' +
+          hyphons + '\n\n';
+  
+  if(desc){
+    if(desc instanceof Array) for(i = 0;i < desc.length;i++){
+      txt += desc[i] + '\n';
+    }else txt += desc + '\n';
+    txt += '\n';
+  }
+  
+  if(!info) return txt;
+  
+  k1 = Object.keys(info);
+  for(i = 0;i < k1.length;i++){
+    txt += k1[i] + ':\n\n';
+    
+    if(typeof info[k1[i]] == 'string' || info[k1[i]] instanceof Array){
+      if(info[k1[i]] instanceof Array) for(k = 0;k < info[k1[i]].length;k++){
+        txt += '  ' + info[k1[i]][k] + '\n';
+      }else txt += '  ' + info[k1[i]] + '\n';
+      
+      txt += '\n';
+      continue;
+    }
+    
+    k2 = Object.keys(info[k1[i]]);
+    for(j = 0;j < k2.length;j++){
+      if(k2[j].length > ml) ml = k2[j].length;
+    }
+    
+    for(j = 0;j < k2.length;j++){
+      txt += '  ' + k2[j] + spaces(ml - k2[j].length + 3);
+      if(info[k1[i]][k2[j]] instanceof Array){
+        txt += info[k1[i]][k2[j]][0] + '\n';
+        for(k = 1;k < info[k1[i]][k2[j]].length;k++)
+          txt += spaces(ml + 5) + info[k1[i]][k2[j]][k] + '\n';
+      }else txt += info[k1[i]][k2[j]] + '\n';
+    }
+    
+    txt += '\n';
+  }
+  
+  return txt;
+};
+
